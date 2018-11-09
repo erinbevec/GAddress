7c7891d39833a9846a49a8351eab26a205d87c6d
erinbevec <42977057+erinbevec@users.noreply.github.com>
2018-11-09 13:07:51 -0500

    Add files via upload

---
+++ GAddress/src/GAddress.java
@@ -0,0 +1,31 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+public class GAddress 
+{
+	public static void main(String[] args) throws FileNotFoundException
+	{
+	Scanner file = new Scanner(new File("GAddress.txt"));
+	ArrayList<String> words = new ArrayList<String>();
+	while (file.hasNext())
+	{
+		words.add(file.next());
+	}
+		String largest = words.get(0);
+		double ave = 0.0;
+		double sum = 0.0;
+		for (int i=0; i < words.size(); i++) 
+		{
+			if (words.get(i).length() > largest.length())
+			{
+				largest = words.get(i);
+			}
+			sum = words.get(i).length() + sum;
+			ave = sum/words.size();
+		}
+		System.out.println("The longest word is " + largest);
+		System.out.println("The average word length is " + ave);
+	}
+	}
+
